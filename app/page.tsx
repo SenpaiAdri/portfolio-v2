0abe59373e0440eba51b256b1d59f3e35aa7a6bb
SenpaiAdri <adrian31dg@gmail.com>
2026-02-25 18:53:04 +0800

    updated the hero section to match on my figma design

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,82 @@
-import Image from "next/image";
-
-export default function Home() {
+import { Activity, Maximize } from "lucide-react";
+export default function Test() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="h-screen w-screen flex flex-col">
+      {/* row 1 */}
+      <div className="flex-1 flex flex-row border-b-red-600 border-b-4 border-dashed">
+        <div className="w-[calc(13/21*100%)] h-full border-r-gray-600 border-r-4 border-dashed flex items-end justify-end pb-10 pr-15">
+          <span className="text-gray-500 text-3xl flex">
+            <Activity className="mr-4 text-red-500" size={35} />
+            Welcome to my Portfolio
+          </span>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="w-[calc(8/21*100%)] h-full flex items-center justify-center">
+          <img
+            src="/logo.svg"
+            alt="profile"
+            className="object-cover mx-auto"
+          />
         </div>
-      </main>
-    </div>
+      </div>
+
+      {/* row 2 */}
+      <div className="flex-1 flex flex-row border-b-gray-600 border-b-4 border-dashed">
+        <div className="w-[calc(13/21*100%)] h-full border-r-gray-600 border-r-4 border-dashed flex flex-row items-end justify-end gap-10 p-10">
+          <div className="flex flex-row items-start justify-end gap-10">
+            <span className="text-red-500 text-2xl">---</span>
+            <div className="flex flex-col ">
+              <span className="text-red-500 text-2xl mb-2">[PATH]</span>
+              <span className="text-gray-500 text-2xl">Compute Science Student</span>
+              <span className="text-gray-500 text-2xl">Specialized in Mobile Programming</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-[calc(8/21*100%)] h-full flex flex-col">
+          <div className=" h-full flex flex-row border-b-red-600 border-b-4 border-dashed">
+            <div className="w-3/5 h-full flex flex-col border-r-gray-600 border-r-4 border-dashed">
+              <div className="w-full h-full border-b-red-600 border-b-4 border-dashed">above</div>
+              <div className="w-full h-full">below</div>
+            </div>
+            <div className="w-full h-full flex flex-col text-right p-10">
+              <span className="text-gray-500 text-2xl
+              hover:text-red-500 hover:translate-x-[-10px] transition-all">[HOME]</span>
+              <span className="text-gray-500 text-2xl
+              hover:text-red-500 hover:translate-x-[-10px] transition-all">[PROJECTS]</span>
+              <span className="text-gray-500 text-2xl
+              hover:text-red-500 hover:translate-x-[-10px] transition-all">[ABOUT]</span>
+              <span className="text-gray-500 text-2xl
+              hover:text-red-500 hover:translate-x-[-10px] transition-all">[CONTACT]</span>
+
+            </div>
+          </div>
+          <div className="h-full flex items-end justify-end mr-10 mb-10">
+            <Maximize className="text-red-500" size={35} />
+          </div>
+        </div>
+      </div >
+      <div className="flex-[0.3] flex flex-col border-b-gray-600 border-b-4 border-dashed overflow-hidden relative">
+        <div className="absolute whitespace-nowrap animate-marquee text-[3rem] md:text-[6rem] text-[#18181c] select-none"
+          style={{
+            WebkitTextStroke: '2px #333',
+            color: 'transparent',
+            left: 0,
+            minWidth: '100%',
+          }}>
+          ADRIAN ADRIAN ADRIAN ADRIAN ADRIAN ADRIAN ADRIAN
+        </div>
+        <style>
+          {`
+            @keyframes marquee {
+              0% { transform: translateX(0); }
+              100% { transform: translateX(-50%); }
+            }
+            .animate-marquee {
+              animation: marquee 15s linear infinite;
+            }
+          `}
+        </style>
+      </div>
+    </div >
   );
 }
