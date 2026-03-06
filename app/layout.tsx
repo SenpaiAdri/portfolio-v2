b8112473d975760214692e60275226d55430eab6
SenpaiAdri <adrian31dg@gmail.com>
2026-03-06 11:25:12 +0800

    update: metadata
    - added more infos in the  metadata
    - website now supports openGraph, twitter(X), and robots too

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,60 @@ const brunoAceSC = Bruno_Ace_SC({
 });
 
 export const metadata: Metadata = {
-  title: "Eydriannn",
-  description: "v2 portfolio of Eydrannn",
+  metadataBase: new URL("https://eydriannn.vercel.app"),
+  title: {
+    default: "Eydriannn",
+    template: "%s | Eydriannn",
+  },
+  description: "Personal website of Adrian",
+  keywords: [
+    "Software Engineer",
+    "Web Developer",
+    "React",
+    "Next.js",
+    "Portfolio",
+    "Adrian",
+    "Eydriannn",
+  ],
+  authors: [{ name: "Adrian" }],
+  creator: "Adrian",
+  icons: {
+    icon: "/logo.svg",
+    apple: "/logo.svg",
+  },
+  openGraph: {
+    title: "Eydriannn",
+    description: "Personal website of Adrian",
+    url: "https://eydriannn.vercel.app",
+    siteName: "Eydriannn",
+    images: [
+      {
+        url: "/logo.svg",
+        width: 600,
+        height: 600,
+        alt: "Eydriannn Portfolio Logo",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Eydriannn",
+    description: "Personal website of Adrian",
+    images: ["/logo.svg"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function RootLayout({
